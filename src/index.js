f280c2f6e5df18234c0e7cddcd2da508ccca0cc8
foolberry <poznyak.dmitri@gmail.com>
2023-10-23 21:36:06 +0300

    solve task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result="";    
+    let arrExpr = expr.match(/.{10}/g); //Split expression to symbols
+        //Create array of Morse code
+        let arrCode = arrExpr.map(function(value) {        
+            if (value == '**********')  return ' ';
+            else {       
+                let intArr = value.match(/.{2}/g);
+                return intArr.reduce(function (prev, current) { 
+                    if (current == '10') return prev + '.';
+                    else if (current == '11') return prev + '-';
+                    else return prev;          
+                },"");    
+                }          
+        })
+        //Use Morse table to find symbol code
+        arrCode.forEach(item => {
+          if (item == " ") result += item;
+          else {
+            result += MORSE_TABLE[item];
+          }
+      })
+  
+    return result; 
 }
 
 module.exports = {
